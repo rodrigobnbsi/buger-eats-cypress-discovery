f39f2f24b2b49714c77e055d21577945a055be56
Rodrigo B Nogueira <rodrigob.nogueira@yahoo.com.br>
2022-04-03 13:23:15 -0300

    Ajustando a suite home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe('home page',()=> { 
     it('app deve estar online',()=> { //caso te teste- it
-        cy.viewport(1440,900)
-        cy.visit('https://buger-eats.vercel.app')// cy - comando para acessar api  e ter acesso aos recursos do framework, aqui ele acessa o site do projeto de teste automatizado
+        cy.visit('/')// cy - comando para acessar api  e ter acesso aos recursos do framework, aqui ele acessa o site do projeto de teste automatizado
         cy.get('#page-home main h1').should('have.text','Seja um parceiro entregador pela Buger Eats') // busca slogan h1 da pagina que inspecionei, e deve ter o texto 'Seja um parceiro entregador pela Buger Eats'
     })
 
